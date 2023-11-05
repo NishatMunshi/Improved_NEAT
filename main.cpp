ae40736c9217a49bab2350eab3bf5e1626bfd3fd
Nishat <nishatmunshi9@gmail.com>
2023-11-05 10:35:36 +0530

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -3,30 +3,25 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(2 * WINDOW_DIMENSION, WINDOW_DIMENSION), "NEAT");
-    // window.close();
-    // window.setFramerateLimit(60);
-
     Species mySpecies(NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, POPULATION);
 
-    // while (true)
+    for (unsigned generation = 0; true; ++generation)
     {
-        for (unsigned generation = 0; true; ++generation)
+        sf::Event _event;
+        while (window.pollEvent(_event))
         {
-            sf::Event _event;
-            while (window.pollEvent(_event))
-            {
-                if (_event.type == sf::Event::Closed)
-                    window.close();
-            }
-
-            mySpecies.play_one_generation(window, generation);
-
-            mySpecies.record_result(generation);
-
-            std::cout << "Generation " << generation << " complete\n";
-
-            mySpecies.repopulate();
+            if (_event.type == sf::Event::Closed)
+                window.close();
         }
+
+        mySpecies.play_one_generation(window, generation);
+
+        mySpecies.record_result(generation);
+
+        std::cout << "Generation " << generation << " complete\n";
+
+        mySpecies.repopulate();
     }
+
     return 0;
 }
